1cca017387f7e83cb3837249fac60dfeeec05dd0
Alabakiz <dariowsp23@gmail.com>
2023-05-17 15:14:33 -0300

    Parte Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import Home from "./Paginas/home/Home"
 import './App.css'
 
 function App() {
   return (
-    <>
-      
+    <> 
+
+    <Home/>
+
     </>
   )
 }
