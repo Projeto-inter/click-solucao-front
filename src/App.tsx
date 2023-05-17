d9e21e4f5f18a56b4a61845927f248376be24ba4
Alabakiz <dariowsp23@gmail.com>
2023-05-17 15:42:44 -0300

    Parte 1 Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import Home from "./Paginas/home/Home"
 import './App.css'
+import Footer from "./Components/Estaticos/footer/Footer"
 
 function App() {
   return (
-    <> 
-
-    <Home/>
-
+    <>
+    <Home />
+    <Footer />
     </>
   )
 }
