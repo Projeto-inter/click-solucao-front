24b82a786721d32d16a16638057315f9197a61b8
Daiane Bittencourt <daibittencourt2000@gmail.com>
2023-05-22 15:58:43 -0300

    Correção de bug tela branca

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 export const api = axios.create({
-
-  baseURL: 'https://clicksolucao.onrender.com/'
+baseURL: 'https://clicksolucao.onrender.com/'
 })
+
+export const cadastroUsuario = async (url: string, dados: object, setDados: any) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+  }
 
 export const login = async (url: string, dados: object, setDados: any) => {
   const resposta = await api.post(url, dados)
   setDados(resposta.data.token)
 }
 
-export const cadastrarUsuario = async (url: string, dados: object, setDados: any) => {
-  const resposta = await api.post(url, dados)
-  setDados(resposta.data)
-}
+
