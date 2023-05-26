77796995371ae12733b2d868da96e1b7e63c2c50
Daiane Bittencourt <daibittencourt2000@gmail.com>
2023-05-26 15:29:00 -0300

    Criação do formulário serviço e listagem dos serviços

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import "./App.css";
 import Login from "./Paginas/login/Login";
 import CadastrarUsuario from "./Paginas/cadastrar-user/CadastrarUser";
 import Sobre from "./Paginas/sobre/Sobre";
+import ListaCategorias from "./Components/categorias/listaCategoria/ListaCategorias";
+import FormularioServico from "./Components/servicos/formularioServico/FormularioServico";
 
 function App() {
   return (
@@ -20,6 +22,10 @@ function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/sobre" element={<Sobre />} />
             <Route path="/signin" element={<CadastrarUsuario />} />
+            <Route path="/categorias" element={<ListaCategorias />} />
+            <Route path='/servicos' element={<ListaServicos />} />
+            <Route path='/formularioServico' element={<FormularioServico />} />
+            <Route path='/formularioServico/:id' element={<FormularioServico />} />
           </Routes>
         </div>
         <Footer />
