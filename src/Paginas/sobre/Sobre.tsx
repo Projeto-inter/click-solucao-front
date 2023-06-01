46fe0e1c61848c3b64531086117b96b61b035e35
Julianalopesco <julianalopesmf@gmail.com>
2023-06-01 17:29:53 -0300

    adição texto de sobre

--- src/Paginas/sobre/Sobre.tsx
+++ src/Paginas/sobre/Sobre.tsx
@@ -7,8 +7,9 @@ function Sobre(){
     return(
         <>
             <Box>
-                <h1>Title</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vel porro iusto. Porro pariatur laboriosam voluptates eligendi quasi ut ipsa laudantium illum modi provident sequi hic blanditiis debitis, quam optio? Atque reprehenderit deserunt quia, necessitatibus dolore dicta a illo accusantium quibusdam repellendus minima deleniti quaerat iusto vel adipisci tempore fugiat.</p>
+                <h1>Sobre o projeto</h1>
+                <p>Click Soluções é uma plataforma de e-commerce inovadora que impulsiona a comercialização de serviços, oferecendo renda e autonomia aos participantes. Ao unir vendedores e consumidores, a plataforma facilita a expansão dos negócios e a variedade de opções de serviços. Com foco no empreendedorismo sustentável, promove práticas éticas, soluções ecológicas e apoio a negócios locais, contribuindo para o desenvolvimento sustentável das comunidades. Seja você um empreendedor ou consumidor, o Click Soluções é o destino ideal para negócios e serviços personalizados.</p>
+                <p>O click Soluções é o Projeto Integrador do Grupo 3 da 63ª turma do Bootcamp da Generation Brasil. Nele foi utilizado Java, SpringBoot, Docker, HTML, CSS, JavaScript entre outras ferramentas e linguagens.</p>
             </Box>
         </>
     )
