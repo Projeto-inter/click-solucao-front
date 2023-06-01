c7ae12c817301031961da136dd0e2777a290b8dd
RaffaGouveia <127224003+RaffaGouveia@users.noreply.github.com>
2023-06-01 15:40:11 -0300

    Update Service.ts

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-baseURL: import.meta.env.VITE_AP_URL
+baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastroUsuario = async (url: string, dados: object, setDados: any) => {
