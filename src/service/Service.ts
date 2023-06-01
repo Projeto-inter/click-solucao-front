c167192d2c639235067fadd77ae2a9b421adb0d8
Rafael Gouveia <raffagouveiabarros@gmail.com>
2023-06-01 11:44:10 -0300

    configuração necessária para o deploy

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-baseURL: 'https://clicksolucao.onrender.com/'
+baseURL: import.meta.env.VITE_AP_URL
 })
 
 export const cadastroUsuario = async (url: string, dados: object, setDados: any) => {
